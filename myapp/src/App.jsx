fe9d754f7abe6d6e9a7f1ea2a9a6011cdf6598e6
ConradAML <conradaml@duck.com>
2026-07-13 17:12:06 +0100

    message

---
+++ myapp/src/App.jsx
@@ -0,0 +1,22 @@
+import PlotModule from "react-plotly.js";
+
+export default function App() {
+  const Plot = PlotModule.default;
+
+  return (
+    <Plot
+      data={[
+        {
+          x: ["Labour", "National", "Green", "ACT"],
+          y: [32.0, 27.4, 10.8, 7.6],
+          type: "bar",
+        },
+      ]}
+      layout={{
+        width: 800,
+        height: 600,
+        title: "2023 NZ Election Vote Share",
+      }}
+    />
+  );
+}
