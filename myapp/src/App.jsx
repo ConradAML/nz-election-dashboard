0db66be9040bdabf6c06ffbd2394029a41452110
ConradAML <conradaml@duck.com>
2026-07-14 10:25:41 +0100

    Render Plotly chart directly in React

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,23 +1,39 @@
-import createPlotlyComponent from "react-plotly.js/factory";
+import { useEffect, useRef } from "react";
 import Plotly from "plotly.js/dist/plotly";
 
-const Plot = createPlotlyComponent(Plotly);
-
 export default function App() {
-  return (
-    <Plot
-      data={[
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (!chartRef.current) {
+      return undefined;
+    }
+
+    Plotly.newPlot(
+      chartRef.current,
+      [
         {
           x: ["Labour", "National", "Green", "ACT"],
           y: [32.0, 27.4, 10.8, 7.6],
           type: "bar",
         },
-      ]}
-      layout={{
+      ],
+      {
         width: 800,
         height: 600,
         title: "2023 NZ Election Vote Share",
-      }}
-    />
-  );
+      },
+      {
+        responsive: true,
+      },
+    );
+
+    return () => {
+      if (chartRef.current) {
+        Plotly.purge(chartRef.current);
+      }
+    };
+  }, []);
+
+  return <div ref={chartRef} />;
 }
